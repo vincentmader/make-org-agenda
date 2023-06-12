c4bdd2e2d6899042188dd0d023a619a6848f69cf
Vincent C. Mader <vincent.mader@icloud.com>
2023-06-12 17:16:02 +0200

    implemented writing to config

--- src/config.py
+++ src/config.py
@@ -2,8 +2,15 @@ import os
 from pathlib import Path
 
 
+XDG_CONFIG_HOME = os.environ["XDG_CONFIG_HOME"]
 ORG_HOME = os.environ["ORG_HOME"]
+
 RELEVANT_ORG_SUPER_DIRECTORIES = [
     Path(ORG_HOME, "1_active-main-projects"),
     Path(ORG_HOME, "2_active-side-projects"),
 ]
+
+DOOM_CONFIG = Path(XDG_CONFIG_HOME, "doom", "config.el")
+
+MARKER_START = ";; MARKER_START_ORG_AGENDA_FILE_LIST\n"
+MARKER_END = ";; MARKER_END_ORG_AGENDA_FILE_LIST\n"
--- src/main.py
+++ src/main.py
@@ -1,13 +1,41 @@
+from config import DOOM_CONFIG
+from config import MARKER_START, MARKER_END
 from paths import paths_to_project_directories
 from paths import paths_to_project_org_files
+
+
+def fmt_agenda_line(org_file):
+    org_file = str(org_file)
+    org_file = org_file.replace("\"", "\\\"")
+    org_file = f"       \"{org_file}\"\n"
+    return org_file
+
+
+def write_agenda_files_to_doom_config(project_org_files):
+    with open(DOOM_CONFIG, 'r') as fp:
+        lines = fp.readlines()
+    lines = "".join(lines)
+    split = lines.split(MARKER_START)
+    lines_pre_agenda = split[0]
+    lines_post_agenda = split[1].split(MARKER_END)[1]
+    
+    agenda_lines = [fmt_agenda_line(f) for f in project_org_files]
+    agenda_lines = "".join(agenda_lines)
+
+    lines = lines_pre_agenda
+    lines += MARKER_START
+    lines += agenda_lines
+    lines += MARKER_END
+    lines += lines_post_agenda
+
+    with open("/Users/vinc/.config/doom/config.el2", 'w') as fp:
+        fp.write(lines)
 
 
 def main():
     project_dirs = paths_to_project_directories()
     project_org_files = paths_to_project_org_files(project_dirs)
-    
-    for p in project_org_files:
-        print(p)
+    write_agenda_files_to_doom_config(project_org_files)
 
 
 if __name__ == "__main__":
