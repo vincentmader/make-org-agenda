5740ff3efb1f1b19129d1417dcad3e27609e77a4
Vincent C. Mader <vincent.mader@icloud.com>
2023-06-24 11:51:54 +0200

    added inbox org files to agenda

--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ XDG_CONFIG_HOME = os.environ["XDG_CONFIG_HOME"]
 ORG_HOME = os.environ["ORG_HOME"]
 
 RELEVANT_ORG_SUPER_DIRECTORIES = [
+    Path(ORG_HOME, "0_inbox"),
     Path(ORG_HOME, "1_active-main-projects"),
     Path(ORG_HOME, "2_active-side-projects"),
     Path(ORG_HOME, "3_chores"),
