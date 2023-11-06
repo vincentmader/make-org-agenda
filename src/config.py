d392d549ac8a5183654c3879a484ff6e5092558f
Vincent C. Mader <vincent.mader@icloud.com>
2023-11-06 01:25:55 +0100

    FIX directory paths

--- src/config.py
+++ src/config.py
@@ -6,13 +6,13 @@ XDG_CONFIG_HOME = os.environ["XDG_CONFIG_HOME"]
 ORG_HOME = os.environ["ORG_HOME"]
 
 RELEVANT_ORG_SUPER_DIRECTORIES = [
-    Path(ORG_HOME, "0_inbox"),
-    Path(ORG_HOME, "1_active-main-projects"),
-    Path(ORG_HOME, "2_active-side-projects"),
-    Path(ORG_HOME, "3_chores"),
+    Path(ORG_HOME, "inbox"),
+    Path(ORG_HOME, "projects", "active-main"),
+    Path(ORG_HOME, "projects", "active-side"),
+    Path(ORG_HOME, "projects", "chores"),
 ]
 
-DOOM_CONFIG = Path(XDG_CONFIG_HOME, "doom", "lisp", "org-agenda.el")
+DOOM_CONFIG = Path(os.environ["DOOM_CONF"], "lisp", "org-agenda.el")
 
 MARKER_START = "; MARKER_START_ORG_AGENDA_FILE_LIST\n"
 MARKER_END = "; MARKER_END_ORG_AGENDA_FILE_LIST\n"
